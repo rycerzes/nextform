8f2f7f9dc48148d29bde4a35bd5885e6bac6286f
swappy <swapsnil12@gmail.com>
2023-11-20 13:06:36 +0530

    added index.tsx

---
+++ src/pages/index.tsx
@@ -0,0 +1,258 @@
+import { Amplify } from 'aws-amplify';
+import amplifyconfig from '@/secrets/amplifyconfiguration.json';
+
+Amplify.configure(amplifyconfig);
+
+import { Inter } from "next/font/google";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { motion } from "framer-motion";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+import { registerSchema } from "@/validators/auth";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import React from "react";
+import { cn } from "@/lib/utils";
+import { ArrowRight } from "lucide-react";
+import { useToast } from "@/components/ui/use-toast";
+
+import { post } from 'aws-amplify/api';
+
+async function addReg(data: any) {
+  try {
+    const queryParams = new URLSearchParams({
+      param1: 'dev',
+    });
+
+    const response = await fetch(`https://xs1wsk16ca.execute-api.ap-south-1.amazonaws.com/dev/register`, {
+      method: 'POST',
+      // mode: 'no-cors',
+      headers: {
+        'Content-Type': 'application/json',
+        // 'X-Amz-Date':,
+        // 'Authorization':,
+        // 'X-Api-Key':,
+        // 'X-Amz-Security-Token':
+      },
+      body: JSON.stringify(data, null, 4),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data1 = await response.json();
+    console.log('POST call succeeded:', data1);
+  } catch (error) {
+    console.log('POST call failed:', error);
+  }
+}
+
+const inter = Inter({ subsets: ["latin"] });
+type Input = z.infer<typeof registerSchema>;
+
+export default function Home() {
+  const { toast } = useToast();
+  const [formStep, setFormStep] = React.useState(0);
+  const form = useForm<Input>({
+    resolver: zodResolver(registerSchema),
+    defaultValues: {
+      phone: "",
+      email: "",
+      name: "",
+      roll: "",
+      year: "",
+    },
+  });
+
+  function onSubmit(data: Input) {
+    alert(JSON.stringify(data, null, 4));
+    console.log(data);
+    addReg(data); 
+  }
+
+  return (
+    <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>MLSA_Event Registration</CardTitle>
+          <CardDescription>New MLSA Event Details</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="relative space-y-3 overflow-x-hidden"
+            >
+              <motion.div
+                className={cn("space-y-3", {
+                  // hidden: formStep == 1,
+                })}
+                // formStep == 0 -> translateX == 0
+                // formStep == 1 -> translateX == '-100%'
+                animate={{
+                  translateX: `-${formStep * 100}%`,
+                }}
+                transition={{
+                  ease: "easeInOut",
+                }}
+              >
+                {/* name */}
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Full name</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Enter your name..." {...field} />
+                      </FormControl>
+                      <FormDescription>
+
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                {/* email */}
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Enter your email..." {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                {/* roll */}
+                <FormField
+                  control={form.control}
+                  name="roll"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Roll Number</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Enter your roll number..."
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                {/* phone */}
+                <FormField
+                  control={form.control}
+                  name="phone"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Phone Number</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Enter your phone number..."
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                {/* year */}
+                <FormField
+                  control={form.control}
+                  name="year"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Year of study</FormLabel>
+                      <Select
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}
+                      >
+                        <FormControl>
+                          <SelectTrigger>
+                            <SelectValue placeholder="Choose year" />
+                          </SelectTrigger>
+                        </FormControl>
+                        <SelectContent>
+                          {[1, 2, 3, 4].map((year) => {
+                            return (
+                              <SelectItem value={year.toString()} key={year}>
+                                Year {year}
+                              </SelectItem>
+                            );
+                          })}
+                        </SelectContent>
+                      </Select>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </motion.div>
+
+              <div className="flex gap-2">
+
+                <Button
+                  type="submit"
+                  className={cn({
+                    hidden: formStep == 1,
+                  })}
+                  onClick={() => {
+                    // validation
+                    form.trigger(["name", "email", "roll", "phone", "year"]);
+                    const emailState = form.getFieldState("email");
+                    const nameState = form.getFieldState("name");
+                    const yearState = form.getFieldState("year");
+                    const rollState = form.getFieldState("roll");
+                    const phoneState = form.getFieldState("phone");
+
+                    if (!emailState.isDirty || emailState.invalid) return;
+                    if (!nameState.isDirty || nameState.invalid) return;
+                    if (!yearState.isDirty || yearState.invalid) return;
+                    if (!rollState.isDirty || rollState.invalid) return;
+                    if (!phoneState.isDirty || phoneState.invalid) return;
+
+                  }}
+                >
+                  Submit
+                  <ArrowRight className="w-4 h-4 ml-2" />
+                </Button>
+
+              </div>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
