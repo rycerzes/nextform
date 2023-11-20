b9e0ab889ecbb52192d7eab059bc3b235c59b7d3
swappy <swapsnil12@gmail.com>
2023-11-20 13:06:20 +0530

    app file added

---
+++ src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ThemeToggle } from "@/components/ThemeToggle";
+import { Toaster } from "@/components/ui/toaster";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
+      <div className="min-h-screen">
+        <Component {...pageProps} />
+        <Toaster />
+      </div>
+
+    </NextThemesProvider>
+  );
+}
